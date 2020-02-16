e4ba301489c3e2bd944f396f7ff4256075cb4b33
Mostafa Soufi <mostafa.soufi@hotmail.com>
2020-02-16 12:10:10 +0200

    Update Database to 20200211

--- update.sh
+++
@@ -1,5 +0,0 @@
-rm -rf GeoLite2-City.mmdb.gz
-wget http://geolite.maxmind.com/download/geoip/database/GeoLite2-City.mmdb.gz GeoLite2-City.mmdb.gz
-git add .
-git commit -m "The database updated!"
-git push
